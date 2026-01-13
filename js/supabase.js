5bb126d2fc47e99ef217acfbb64691740b2b7f08
skul-hub <yoelafrido42@gmail.com>
2026-01-13 11:03:21 +0700

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
+<script>
+const supabaseUrl = "https://mwmfsvbhjhyhmuppynyu.supabase.co";
+const supabaseKey = "sb_publishable_XGFmy6H91Lf65Id5gQsdZQ_WjSp-3iG";
+
+window.supabase = supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+</script>
